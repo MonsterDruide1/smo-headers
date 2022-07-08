f37e42c502cae2c9c179a49a43c5b4f8fc7c5c9e
MonsterDruide1 <5958456@gmail.com>
2022-07-08 02:27:46 +0200

    Add test for main headers

---
+++ compile.sh
@@ -0,0 +1,10 @@
+/home/monsterdruide1/OdysseyDecomp/toolchain/clang-3.9.1/bin/clang++ \
+    --target=aarch64-linux-elf \
+    --sysroot=/home/monsterdruide1/OdysseyDecomp/toolchain/musl \
+    -DNON_MATCHING \
+    -I. -Iaarch64 -INintendoSDK/include -Isead/include -Iagl/include \
+    -DVER_100 -O3 -g   -D SWITCH -D NNSDK -D MATCHING_HACK_NX_CLANG -mcpu=cortex-a57+fp+simd+crypto+crc \
+    -mno-implicit-float -stdlib=libc++ -fPIC -fstandalone-debug -fcolor-diagnostics -Wno-unused-command-line-argument \
+    -fdebug-prefix-map=/tmp=/home/monsterdruide1/smo-headers/build -fno-rtti -fno-exceptions -Wall -Wextra \
+    -Wdeprecated -Wno-unused-parameter -Wno-unused-private-field -fno-strict-aliasing -Wno-invalid-offsetof -std=c++1z -c test.cpp
+
---
+++ generate.sh
@@ -0,0 +1,13 @@
+HEADER=all_headers.h
+echo "#ifndef __ALL_HEADERS__" > $HEADER
+echo "#define __ALL_HEADERS__" >> $HEADER
+# don't check NintendoSDK, as that contains 4.0.1-only files
+for file in $(find aarch64 agl al game rs sead -name '*.h');
+#for file in $(find online-headers -name '*.h')
+do
+    # ignore Wii-specific files
+    # ignore ScopeGuard, as that uses <optional> (C++17)
+    if [[ $file == *"cafe"* || $file == *"seadScopeGuard.h" ]]; then continue; fi
+    echo "#include \"$file\"" >> $HEADER
+done
+echo "#endif" >> $HEADER
